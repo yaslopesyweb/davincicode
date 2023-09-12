f2bac2811e0a8e66027e76188f5d4898b08b56e2
Yasmin Lopes <slahyasmin@gmail.com>
2023-09-12 18:38:24 -0300

    Adicionando plus-title e space

---
+++ src/app/componentes/space.js
@@ -0,0 +1,14 @@
+"use client"
+import styled from "styled-components";
+
+// Estilizar Espaço
+const Space = styled.div`
+   padding: 13px;
+`;
+
+// Espaço
+export default function Espaco() {
+    return (
+            <Space></Space>
+    );
+}
